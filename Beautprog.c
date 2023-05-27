b7ab17578a53f0555f91a64c8101a0b82484e614
AromeGab <aromegabrieljnr@gmail.com>
2023-05-27 15:40:54 -0700

    Task 0 carried out by Arome Gabriel

---
+++ Beautprog.c
@@ -0,0 +1,18 @@
+/**
+ * main - is the begining point of this program
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 50; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+
+	return (0);
+}
